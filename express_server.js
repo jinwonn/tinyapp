e776125ca11d6d1fcfcc834355c7b539c6bb2974
Daniel <d.t.nguyen@live.com>
2018-10-05 04:09:34 +0000

    implemented sign in form and log out button and corresponding functions to create and clear cookies

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,7 +23,6 @@ function generateRandomString(length) {
   
     return randomshort;
 }
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,22 +41,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  let templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars)
 });
 
+//for creation of new short and long URL pair
 app.post("/urls", (req, res) => {
   var shortURL = generateRandomString(6)
   let longURL = req.body.longURL
   urlDatabase[shortURL] = longURL
-  
-  // urlDatabase[shortURL] = longURL
+
   res.redirect(`http://localhost:${PORT}/urls/` + shortURL);
-  // console.log(req.body);  // debug statement to see POST parameters
   // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
@@ -66,17 +66,21 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
 
+//post for deleting entry pair of shortURL and longURL
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
+
+//post for updating the longURL value of shortURL
 app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id
   let longURL = req.body.longURL
@@ -87,10 +91,16 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`http://localhost:${PORT}/urls/` + shortURL);
 });
 
+//creates cookie for inputted username
 app.post("/login", (req, res) => {
   let username = req.body.username
-console.log(username)
   res.cookie('username', req.body.username)
 
+  res.redirect(`http://localhost:${PORT}/urls/`);
+});
+
+//POST for logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect(`http://localhost:${PORT}/urls/`);
 });
